39ae963a6f2538e4beb4c8f99b4cb435999e2b12
Erik Heath <erikheath8@gmail.com>
2020-11-21 16:53:19 -0500

    Final Project Commit

---
+++ Greeter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace weekTwoChallenge2
+{
+    class Greeter
+    {
+         public string FirstName { get; set; }
+        public string getFirstName()
+        {
+            
+            Console.WriteLine("Hello, Please enter your First Name....\n");
+            FirstName = Console.ReadLine();
+            Thread.Sleep(1000);
+            return FirstName;
+        }
+
+        public void Greeting()
+        {
+            string greetFirst = FirstName;
+            Console.WriteLine($"\nHello {greetFirst}.");
+            Thread.Sleep(1500);
+        }
+
+         public void Greeting(string greetFirst)
+        {
+            Console.WriteLine($"\nHello {greetFirst}.");
+            Thread.Sleep(1500);
+        }
+
+
+        public void Farewell()
+        {
+            string farFirstName = FirstName;
+           Console.WriteLine($"\nGoodbye {farFirstName}.");
+            Thread.Sleep(1500);
+        }
+
+        public void Farewell(string farFirstName)
+        {
+            Console.WriteLine($"\nGoodbye {farFirstName}.");
+            Thread.Sleep(1500);
+        }
+       
+        public void TimeDayGreet()
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour >= 1 && hour <= 12)
+            {
+                Console.WriteLine("\nGoodMorning.");
+                Thread.Sleep(2500);
+            }
+            else if (hour >= 12 && hour <= 16)
+            {
+                Console.WriteLine("\nGood Afternoon.");
+                Thread.Sleep(2500);
+            }
+            else if (hour >= 16 && hour <= 21)
+            {
+                Console.WriteLine("\nGood Evening.");
+                Thread.Sleep(2500);
+            }
+            else if (hour >= 21 && hour <= 24)
+            {
+                Console.WriteLine("\nGood Night.");
+                Thread.Sleep(2500);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace weekTwoChallenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeter ps = new Greeter();
+            ps.getFirstName();
+            ps.Greeting();            
+            ps.TimeDayGreet();
+            ps.Farewell();
+                                 
+        }
+    }
+}
